af07db702664485a0cdd0069664214a91460914b
SLey3 <ghub4127@gmail.com>
2024-04-17 06:03:23 -0700

    minor changes

--- realtorlistings/api/databaseconnect.php
+++ realtorlistings/api/databaseconnect.php
@@ -13,16 +13,18 @@
         private $server;
         private $user;
         private $pass;
+        private $port;
 
         public function __construct()
         {
             $this->server = $_ENV['AWS_DATABASE_URL'];
             $this->user = $_ENV['AWS_DATABASE_USER'];
             $this->pass = $_ENV['AWS_DATABASE_PWD'];
+            $this->port = $_ENV['AWS_DATABASE_PORT'];
         }
 
         public function connect() {
-            $conn = mysqli_connect($this->server, $this->user, $this->pass, 'realtorlistingsdb');
+            $conn = mysqli_connect($this->server, $this->user, $this->pass, 'realtorlistingsdb', $this->port);
 
             if (mysqli_connect_errno()) {
                 return 503; // HTTP code for Service unavailable
--- realtorlistings/frontend/src/components/Home.tsx
+++ realtorlistings/frontend/src/components/Home.tsx
@@ -19,13 +19,13 @@ const Home: React.FC = () => {
                     <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl">Finding your new Home</h1>
                     <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at RealtorListings, we believe you deserve the right home and should find it easily.</p>
                     <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-                        <a href={cookies.user ? "/listings" : "/register"} className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                        <a className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900" href={cookies.user ? "/listings" : "/register"}>
                             {cookies.user ?  "Listings" : "Get started" }
                             <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                             </svg>
                         </a>
-                        <a href="/about" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white border border-white rounded-lg hover:text-gray-900 sm:ms-4 hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+                        <a className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white border border-white rounded-lg hover:text-gray-900 sm:ms-4 hover:bg-gray-100 focus:ring-4 focus:ring-gray-400" href="/about">
                             Learn more
                         </a>  
                     </div>
