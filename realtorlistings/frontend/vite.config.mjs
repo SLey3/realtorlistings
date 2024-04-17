c21457f72b2ad09325cf117c86db9bf1de4958fd
Sergio Ley <44455382+SLey3@users.noreply.github.com>
2024-04-17 00:09:44 -0700

    Update vite.config.mjs

--- realtorlistings/frontend/vite.config.mjs
+++ realtorlistings/frontend/vite.config.mjs
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react-swc';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-    // depending on your application, base can also be "/"
+
     base: '',
     plugins: [react(), viteTsconfigPaths()],
     server: {    
         // this ensures that the browser opens upon server start
         open: true,
-        // this sets a default port to 3000  
+        // this sets a default port to 8000  
         port: 3000, 
         proxy: {
             '/api' : {
