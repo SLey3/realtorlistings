a26a03643a94df283da21d0e55c4d1b4996c3e3a
SLey3 <ghub4127@gmail.com>
2024-04-24 15:24:29 -0700

    vercel routes fix + comment out proxy setup for vite config

--- realtorlistings/frontend/vite.config.mjs
+++ realtorlistings/frontend/vite.config.mjs
@@ -11,13 +11,13 @@ export default defineConfig({
         open: true,
         // this sets a default port to 3000  
         port: 3000, 
-        proxy: {
-            '/api' : {
-                target: 'https://api.realtor-listings.com',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ''),
-            }
-        }
+        // proxy: {
+        //     '/api' : {
+        //         target: 'https://api.realtor-listings.com',
+        //         changeOrigin: true,
+        //         rewrite: (path) => path.replace(/^\/api/, ''),
+        //     }
+        // }
     },
     build: {
         chunkSizeWarningLimit: 1000,
