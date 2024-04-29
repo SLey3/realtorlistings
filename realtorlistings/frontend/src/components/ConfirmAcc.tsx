403c2928e27040f80e02d37cd20ecc8025cce6c0
SLey3 <ghub4127@gmail.com>
2024-04-29 03:01:37 -0700

    removed useEffect from confirmacc

--- realtorlistings/frontend/src/components/ConfirmAcc.tsx
+++ realtorlistings/frontend/src/components/ConfirmAcc.tsx
@@ -10,7 +10,7 @@ const ConfirmAccount: React.FC = () => {
 
     useEffect(() => {
         confirmAccount();
-    }, [params]);
+    }, []);
 
     const confirmAccount = () => {
         axios.post(`/api/confirmacc`, {
