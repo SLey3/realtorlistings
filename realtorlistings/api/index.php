125bba88de8f851462557d8218aeb07b6086505d
SLey3 <ghub4127@gmail.com>
2024-04-28 21:48:29 -0700

    edited CORS rules for the api

--- realtorlistings/api/index.php
+++ realtorlistings/api/index.php
@@ -41,7 +41,7 @@
     $app->add(function ($request, $handler) {
         $response = $handler->handle($request);
         return $response
-                ->withHeader('Access-Control-Allow-Origin', 'https://www.realtor-listings.com, https://realtorlistings-frontend-git-main-sergio-leys-projects.vercel.app')
+                ->withHeader('Access-Control-Allow-Origin', '*')
                 ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
                 ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     });
